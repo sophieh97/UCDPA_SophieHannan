9e0373ff5be478f4f2bdca7c8b48f3d6e05973b5
sophieh97 <sophiehannan97@gmail.com>
2021-03-29 19:35:24 +0100

    Project Commencement

---
+++ Notes.py
@@ -0,0 +1,8 @@
+#https://www.kaggle.com/pavansubhasht/ibm-hr-analytics-attrition-dataset
+#https://www.kaggle.com/vjchoudhary7/hr-analytics-case-study?select=employee_survey_data.csv - This one very good!!
+
+#analyse two companies data to uncover what factors are contributing to employee attrition and try to prediction turnover for both
+#look at data before decision
+#any commonalities
+#make sure aloy of values
+#one database multiple merges need
