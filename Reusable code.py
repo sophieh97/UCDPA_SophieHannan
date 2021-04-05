23bee8782465b5363f84fccdb03a9a87586aea39
sophieh97 <sophiehannan97@gmail.com>
2021-04-05 20:43:26 +0100

    Reusable code.py

--- Reusable code.py
+++ Reusable code.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def import_data(filename):
     data = pd.read_csv(filename)
     print(data)
@@ -17,4 +18,3 @@ data = import_data("XYZ Company/general_data.csv")
 data = import_data("XYZ Company/in_time.csv")
 data = import_data("XYZ Company/manager_survey_data.csv")
 data = import_data("XYZ Company/out_time.csv")
-
