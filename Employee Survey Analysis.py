92a80063b4f021f6373f29261f26ba2c4d5efcea
sophieh97 <sophiehannan97@gmail.com>
2021-04-08 15:31:16 +0100

    8.04.2021 - Employee Survey Analysis.py

--- Employee Survey Analysis.py
+++ Employee Survey Analysis.py
@@ -14,34 +14,44 @@ def import_data(filename):
     print(data.index)
     return data
 
+
 # Analysing data
 # Missing Values
-def cleandata(data):
+def clean_data(data):
     data = data.fillna("null")
     print(data.isnull().sum())
     print(data.head())
     print(data.shape)
-    return (data)
+    return data
+
+
+# Replace using a dictionary
+def my_dict(columns):
+    my_dict = data[columns].replace({1: "Low", 2: "Medium", 3: "High", 4: "Very High"})
+    print(my_dict.head())
+    return data
+
 
 # Sorting employee_survey from lowest to highest
 def sort_values(columns):
-    employee_survey_replace = data(column).replace({1:"Low", 2:"Good", 3:"Excellent", 4:"Outstanding"})
-    employee_survey_data = data.sort_values(columns)
-    print(employee_survey_data.head())
-    print(employee_survey_data.shape)
-    return(data)
+    sort_values = data.sort_values(columns)
+    print(sort_values.head())
+    print(sort_values.shape)
+    return data
+
 
 # Counting Data
 def value_counts(columns):
-    employee_survey_dataset = data[columns].value_counts()
-    print(data.isnull().sum())
-    print(employee_survey_dataset.head())
-    return(data)
+    value_counts = data[columns].value_counts()
+    print(value_counts.isnull().sum())
+    print(value_counts.head())
+    return data
+
 
 data = import_data("XYZ Company/employee_survey_data.csv")
-cleandata(data)
+clean_data(data)
+my_dict(["EnvironmentSatisfaction","JobSatisfaction", "WorkLifeBalance"])
 sort_values(["EnvironmentSatisfaction", "JobSatisfaction", "WorkLifeBalance"])
 value_counts("EnvironmentSatisfaction")
 value_counts("JobSatisfaction")
 value_counts("WorkLifeBalance")
-
