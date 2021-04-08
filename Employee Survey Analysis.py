9c9d732324464b8ec0456937123e9ee50559a16e
sophieh97 <sophiehannan97@gmail.com>
2021-04-08 17:44:47 +0100

    8.04.2021 - Employee Survey Analysis.py

--- Employee Survey Analysis.py
+++ Employee Survey Analysis.py
@@ -24,19 +24,19 @@ def clean_data(data):
     print(data.shape)
     return data
 
-
-# Replace using a dictionary
-def my_dict(columns):
-    my_dict = data[columns].replace({1: "Low", 2: "Medium", 3: "High", 4: "Very High"})
-    print(my_dict.head())
-    return data
-
-
 # Sorting employee_survey from lowest to highest
 def sort_values(columns):
     sort_values = data.sort_values(columns)
     print(sort_values.head())
     print(sort_values.shape)
+    return data
+
+
+# Replace using a dictionary
+def my_dict(columns):
+    my_dict = data[columns].replace({1: "Low", 2: "Medium", 3: "High", 4: "Very High"})
+    print(my_dict.head())
+    print(data.head())
     return data
 
 
@@ -50,8 +50,19 @@ def value_counts(columns):
 
 data = import_data("XYZ Company/employee_survey_data.csv")
 clean_data(data)
-my_dict(["EnvironmentSatisfaction","JobSatisfaction", "WorkLifeBalance"])
 sort_values(["EnvironmentSatisfaction", "JobSatisfaction", "WorkLifeBalance"])
+my_dict(["EnvironmentSatisfaction","JobSatisfaction", "WorkLifeBalance"])
 value_counts("EnvironmentSatisfaction")
 value_counts("JobSatisfaction")
 value_counts("WorkLifeBalance")
+#check if employee ID still in it if not do separtely like below
+
+# Manager Survey Analysis
+data = import_data("XYZ Company/manager_survey_data.csv")
+data ["JobInvolvement"] = data["JobInvolvement"].replace({1:"Low", 2:"Medium", 3:"High", 4:"Very High"})
+data ["PerformanceRating"] = data["PerformanceRating"].replace({1:"Low", 2:"Good", 3:"Excellent", 4:"Outstanding"})
+print(data.head())
+value_counts("JobInvolvement")
+value_counts("PerformanceRating")
+
+# In Time
--- Extra.py
+++ Extra.py
@@ -1,2 +1,12 @@
 
 
+# Set index Employee ID
+def set_index(column):
+    set_index: data.set_index(column)
+    print(set_index)
+    return data
+
+# Below works
+data ["JobInvolvement"] = data["JobInvolvement"].replace({1:"Low", 2:"Medium", 3:"High", 4:"Very High"})
+data ["PerformanceRating"] = data["PerformanceRating"].replace({1:"Low", 2:"Good", 3:"Excellent", 4:"Outstanding"})
+print(data.head())
